4218b626cab186eee458e0bffb3779c935fd801d
mhmmdwibi <mohwibisonoarifin@smkwikrama.sch.id>
2023-06-11 06:10:09 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Sejarah pengembangan Teknologi</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="assets/css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#page-top">
+                    <img src="assets/img/logo.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#scroll">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#tap">Kembali</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <section id="tap">
+            <header class="masthead text-center text-white" style="background-image: url('assets/img/mt-1.jpg');">
+                <div class="masthead-content fade-in">
+                    <div class="container px-5">
+                        <h1 class="masthead-heading mb-0">Sejarah Perkembangan Teknologi</h1>
+                        <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Selanjutnya</a>
+                    </div>
+                </div>
+            </header>
+        </section>
+        <!-- Content section 1-->
+        <section id="scroll">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/00.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <p>Halo semuanya! Pada kesempatan kali ini, kita akan menjelajahi sejarah perkembangan teknologi. Teknologi telah menjadi bagian tak terpisahkan dari kehidupan kita sehari-hari, dan mempengaruhi hampir semua aspek kehidupan manusia. Dalam materi ini, kita akan melihat perkembangan teknologi dari masa ke masa, dan bagaimana inovasi-inovasi tersebut telah mengubah dunia kita.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content section 2-->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5"><img class="img-fluid rounded" src="assets/img/01.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <h2>Era Pra-Sejarah</h2>
+                            <p>Pada awalnya, manusia hidup dalam era pra-sejarah tanpa teknologi canggih. Mereka bergantung pada alam untuk bertahan hidup dan menggunakan alat-alat sederhana seperti batu untuk memenuhi kebutuhan dasar mereka. Ini adalah era pembentukan fondasi awal manusia dalam mengembangkan keterampilan dan pengetahuan mereka.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content section 3-->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded" src="assets/img/02.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2>Perkembangan Awal</h2>
+                            <p>  Revolusi Industri pada abad ke-18 dan ke-19 merupakan periode penting dalam sejarah perkembangan teknologi. Penggunaan mesin uap sebagai sumber energi memberikan dorongan besar bagi sektor industri. Pabrik-pabrik dan mesin-mesin baru dikembangkan, menggantikan pekerjaan manual yang sebelumnya dilakukan oleh manusia atau hewan.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content section 4-->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5"><img class="img-fluid rounded" src="assets/img/03.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <h2>Perkembangan Teknologi Modern</h2>
+                            <p>Komunikasi dan Transportasi: Pada abad ke-20, komunikasi dan transportasi mengalami kemajuan yang luar biasa. Penemuan telegraf dan telepon memungkinkan orang untuk berkomunikasi dengan cepat dari jarak jauh. Kemudian, radio dan televisi mengubah cara kita mendapatkan informasi dan hiburan. Kemajuan dalam bidang transportasi juga terjadi, seperti pengembangan pesawat terbang, mobil, dan kereta api.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content section 5-->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded " src="assets/img/04.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2>Era Digital</h2>
+                            <p>
+                                Era digital adalah periode waktu di mana teknologi informasi dan komunikasi, terutama komputer dan internet, telah menjadi faktor dominan dalam kehidupan manusia. Era ini ditandai oleh pergeseran dari sistem fisik dan analog ke sistem digital. Penggunaan teknologi digital telah mengubah cara kita bekerja, berkomunikasi, mendapatkan informasi, hiburan, dan bahkan menjalani kehidupan sehari-hari.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section><br><br>
+        <!-- kesimpulan-->
+        <div class="container px-4 px-lg-5 text-center">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-lg-10">
+                    <h2 class="fade-in" style="font-family: 'Times New Roman', Times, serif;">Kesimpulan</h2>
+                    <p class="lead mb-5 fade-in" style="font-family: 'Times New Roman', Times, serif;">
+                        Dalam perjalanan sejarah perkembangan teknologi, kita telah menyaksikan perubahan yang luar biasa dalam cara kita hidup dan bekerja. Dari alat sederhana pra-sejarah hingga era digital yang kompleks, teknologi terus berkembang dan mengubah dunia kita. Dalam menghadapi masa depan, penting bagi kita untuk terus terbuka terhadap inovasi dan adaptasi dengan perkembangan teknologi. Masa depan teknologi mungkin melibatkan kemajuan dalam bidang kecerdasan buatan, robotika, teknologi wearable, dan integrasi yang lebih dalam antara manusia dan mesin.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="py-5 bg-black">
+            <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; Moh Wibisono Arifin 2023</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
